8341b8ecd8a7b044684ed0ecb65ab4b17e8c7e4e
idrissiamale <aidrissi001@gmail.com>
2020-01-03 19:11:49 +0100

    Creation of Main class In ReadSymptomDataFromFile class: - add of getters and setters; - add of the path of symptoms.txt into private String filePath attribute; - add of private ArrayList<String> result attribute.

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -8,40 +8,60 @@ import java.util.List;
 
 /**
  * Simple brute force implementation
- *
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
+    private String filePath = "C:\\Users\\abdelaziz\\IdeaProjects\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt";
+    private ArrayList<String> result;
 
-	private String filepath;
-	
-	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
-	 */
-	public ReadSymptomDataFromFile (String filepath) {
-		this.filepath = filepath;
-	}
-	
-	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
-		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
-				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
-				}
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return result;
-	}
+    /**
+     * @param filePath a full or partial path to file with symptom strings in it, one per line
+     */
+    public ReadSymptomDataFromFile(String filePath) {
+        this.filePath = filePath;
+    }
 
+    public ReadSymptomDataFromFile() {
+
+    }
+
+    // Reading a file (symptoms.txt) and adding it into an ArrayList (result)
+    @Override
+    public List<String> GetSymptoms() {
+        result = new ArrayList<String>();
+
+        if (filePath != null) {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(filePath));
+                String line = reader.readLine();
+
+                while (line != null) {
+                    result.add(line);
+                    line = reader.readLine();
+                }
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    //getters and setters
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public ArrayList<String> getResult() {
+        return result;
+    }
+
+    public void setResult(ArrayList<String> result) {
+        this.result = result;
+    }
 }
